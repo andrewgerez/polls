9797ca23ecbf1be21ea98b70278d8b7f027235ca
Andrew Gerez <andrewgerez18@gmail.com>
2024-02-07 22:31:16 -0300

    chore: fastify & docker setup

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,6 +2,10 @@ import fastify from "fastify"
 
 const app = fastify()
 
+app.get('/hello', () => {
+  return 'Hello'
+})
+
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
 })
