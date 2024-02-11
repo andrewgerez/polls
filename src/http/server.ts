a04a2d1024e17bbb61e5b936b7109cb8cbb30d94
Andrew Gerez <andrewgerez18@gmail.com>
2024-02-11 16:39:42 -0300

    feat: poll results route

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,6 +4,7 @@ import websocket from "@fastify/websocket"
 import { createPoll } from "./routes/create-poll"
 import { getPoll } from "./routes/get-poll"
 import { voteOnPoll } from "./routes/vote-on-poll"
+import { pollResults } from "./ws/poll-results"
 
 const app = fastify()
 
@@ -17,6 +18,7 @@ app.register(websocket)
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+app.register(pollResults)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
