f461ca628aa4d704caf69491e84d6ca67ee384c3
Andrew Gerez <andrewgerez18@gmail.com>
2024-02-11 15:40:48 -0300

    chore: fastify cookies

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -1,6 +1,7 @@
-import z from "zod";
-import { prisma } from "../../lib/prisma";
-import { FastifyInstance } from "fastify";
+import z from "zod"
+import { randomUUID } from "node:crypto"
+import { prisma } from "../../lib/prisma"
+import { FastifyInstance } from "fastify"
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (request, reply) => {
@@ -14,6 +15,8 @@ export async function voteOnPoll(app: FastifyInstance) {
 
     const { pollId } = voteOnPollParams.parse(request.params)
     const { pollOptionId } = voteOnPollBody.parse(request.body)
+
+    const sessionId = randomUUID()
 
     return reply.status(201).send()
   })
